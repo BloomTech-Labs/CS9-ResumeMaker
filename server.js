bffbfe07ea66671b8fb6aae369f6301813ceebd8
ktan114 <22802321+ktan114@users.noreply.github.com>
2018-08-14 16:27:56 -0400

    Initialized server

--- server.js
+++ server.js
@@ -4,3 +4,8 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
+
+const server = express();
+
+const port = process.env.PORT || 3333;
+server.listen(port, () => console.log(`Server running on port: ${port}`))
