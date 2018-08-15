62313a555ac93ad07a2fb4df760bc7952398b982
Cole Phillips <nphillips78@gmail.com>
2018-08-15 16:17:04 -0400

    basic context components set up, still adding

---
+++ client/src/context.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react'
+
+const UserContext = React.createContext({
+  isLoggedIn: true // TODO:need this to access user data while user is logged in
+})
+
+export class UserContextProvider extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      isLoggedIn: true
+    }
+  }
+
+  render() {
+    return (
+      <UserContext.Provider value={{
+        isLoggedIn: this.state.isLoggedIn
+      }}>
+      </UserContext.Provider>
+    )
+  }
+}
+
+const SidebarContext = React.createContext({
+  showSidebar: true
+})
+
+export class SidebarContext extends Component {
+  constructor(props) {
+    super(props)
+
+  this.state = {
+    showSidebar: true 
+    }
+  }
+
+  render() {
+    return(
+      <SidebarContext.Provider calue ={{
+        showSidebar: this.state.showSidebar
+      }}>
+      </SidebarContext.Provider>
+    )
+  }
+
+}
+
+export default UserContext; SidebarContext
