f94596cdc4425cea6980f2cb0339217e1f2f594a
Cruise Brantley <cruise.brantley@gmail.com>
2018-08-16 17:09:21 -0500

    Fixed a lot of warnings, login now pulls all the data into context

--- client/src/components/resumes.js
+++ client/src/components/resumes.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import { Route } from "react-router-dom";
 import Sidebar from "./subComponents/sidebar"
 import Navbar from "./subComponents/navbar";
 import ResumeCard from "./subComponents/resumeCard"
