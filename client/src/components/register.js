9b202fdc23e1301e0a4d7ce9a7927e4bc7ab1be9
nickbrenn <nbren@tutanota.com>
2018-08-21 18:54:29 -0400

    Made a functioning registration component based on the login one

--- client/src/components/register.js
+++ client/src/components/register.js
@@ -1,58 +1,104 @@
 import React, { Component } from "react";
+import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import "./CSS/login.css";
+import axios from "axios";
+
+const urls = require("../config.json");
 
 class Register extends Component {
   state = {
     email: "",
     password: "",
+    confirmPassword: "",
     username: "",
     invalidCredentials: false
   };
 
+  validateForm() {
+    return this.state.email.length > 0 && this.state.password.length > 0;
+  }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.id]: event.target.value
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    axios
+      .post(`${urls[urls.basePath]}/users/register`, {
+        username: this.state.username,
+        email: this.state.email,
+        password: this.state.password
+      })
+      .then(response => {
+        if (response.data.token) {
+          const userData = response.data.user;
+          console.log(userData);
+          localStorage.setItem("token", response.data.token);
+          // this.props.context.actions.setLogin(userData);
+
+          console.log(this.props.context.userInfo);
+          this.props.history.push("/login");
+        } else this.setState({ invalidCredentials: true, password: "" });
+      })
+      .catch(err => {
+        console.log("err", err);
+        localStorage.removeItem("token");
+        this.setState({ invalidCredentials: true, password: "" });
+      });
+  };
+
   render() {
     return (
-      <div className="App">
-        <h3>Please create an account. </h3>
-        <form class="signup">
-          <div class="form-group">
-            <input type="text" class="form-control" placeholder="Username" />
-          </div>
-          <div class="form-group">
-            <input
+      <div className="Login">
+        <form onSubmit={this.handleSubmit}>
+          <FormGroup controlId="username" bsSize="large">
+            <ControlLabel>Username</ControlLabel>
+            <FormControl
+              autoFocus
+              type="username"
+              value={this.state.username}
+              onChange={this.handleChange}
+            />
+          </FormGroup>
+          <FormGroup controlId="email" bsSize="large">
+            <ControlLabel>Email</ControlLabel>
+            <FormControl
+              autoFocus
               type="email"
-              class="form-control"
-              placeholder="Email Address"
+              value={this.state.email}
+              onChange={this.handleChange}
             />
-          </div>
-
-          <div class="form-group">
-            <input
+          </FormGroup>
+          <FormGroup controlId="password" bsSize="large">
+            <ControlLabel>Password</ControlLabel>
+            <FormControl
               type="password"
-              class="form-control"
-              placeholder="Password"
+              value={this.state.password}
+              onChange={this.handleChange}
             />
-          </div>
-          <div class="form-group">
-            <input
-              type="password"
-              class="form-control"
-              placeholder="Confirm Password"
+          </FormGroup>
+          <FormGroup controlId="confirmPassword" bsSize="large">
+            <ControlLabel>Confirm password</ControlLabel>
+            <FormControl
+              type="confirmPassword"
+              value={this.state.confirmPassword}
+              onChange={this.handleChange}
             />
-          </div>
-          <div class="form-group">
-            <div class="checkbox">
-              <label>
-                <input type="checkbox" />
-                <span>
-                  Please accept the terms and conditions to proceed with your
-                  request.
-                </span>
-              </label>
-            </div>
-            <div class="form-group">
-              <input type="submit" class="btn btn-success " value="SUBMIT" />
-            </div>
-          </div>
+          </FormGroup>
+          <Button
+            block
+            bsSize="large"
+            disabled={!this.validateForm()}
+            type="submit"
+          >
+            Register
+          </Button>
         </form>
+        {this.state.invalidCredentials ? <h3>Invalid Credentials</h3> : null}
       </div>
     );
   }
