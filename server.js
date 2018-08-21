4df7ea17abd26f5df35a9c3fdbccb22943e3d94b
ktan114 <ktaneight@gmail.com>
2018-08-21 13:22:48 -0400

    Add cors options in comments  that may be used during production

--- server.js
+++ server.js
@@ -28,6 +28,14 @@ require("./user/config_passport.js")(passport);
 server.use(express.json());
 server.use(express.urlencoded({ extended: false }));
 
+// const whitelist = ['https://labs-resume-maker.firebaseapp.com/'];
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     whitelist.indexOf(origin) !== -1 ? callback(null, true) : callback(new Error('Access Denied'))
+//   }
+// }
+// server.use(cors(corsOptions));
+
 // Route for editing/adding/deleting users
 const UserRouter = require("./user/UserRoutes.js");
 server.use("/users", UserRouter);
