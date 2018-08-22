f2118595dbce88c235b908c11a48b2b1b5296322
ktan114 <ktaneight@gmail.com>
2018-08-22 17:26:41 -0400

    Removed hbs from server and delete test route

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const passport = require("passport");
-const hbs = require('hbs');
 require("dotenv").config();
+
+const UserRouter = require("./user/UserRoutes.js");
+const StripeRouter = require("./stripe/StripeRouter.js");
+
 
 // Connect To mLab
 mongoose
@@ -16,9 +19,6 @@ mongoose
 
 // Initialize Server
 const server = express();
-
-server.set('view engine', 'hbs');
-server.set('views', __dirname + '/views');
 
 // Initialize passport authentication
 server.use(passport.initialize());
@@ -37,18 +37,9 @@ server.use(cors());
 // }
 // server.use(cors(corsOptions));
 
-// Route for editing/adding/deleting users
-const UserRouter = require("./user/UserRoutes.js");
+// Routes
 server.use("/users", UserRouter);
-
-// Route for Stripe
-const StripeRouter = require("./stripe/StripeRouter.js");
 server.use('/pay', StripeRouter);
-
-// Initial GET route
-server.get("/", (req, res) => {
-  res.json({ Message: "Hello World" });
-});
 
 const port = process.env.PORT || 3333;
 server.listen(port, () => console.log(`Server running on port: ${port}`));
