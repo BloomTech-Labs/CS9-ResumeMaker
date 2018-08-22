c1688b9bb46856407db070b0b0c8d208c1c5100c
ktan114 <ktaneight@gmail.com>
2018-08-22 17:50:06 -0400

    Added styling to the fields, change button text

--- client/src/components/billing.js
+++ client/src/components/billing.js
@@ -1,27 +1,32 @@
 import React, { Component } from "react";
 import { CardElement, injectStripe } from "react-stripe-elements";
-import axios from 'axios';
+import axios from "axios";
+
 import Sidebar from "./subComponents/sidebar";
 import Navbar from "./subComponents/navbar";
+import "./CSS/billing.css";
 
 class Billing extends Component {
   state = {
     complete: false
-  }
+  };
 
   submit = async () => {
-    let { token } = await this.props.stripe.createToken({ email: this.props.context.userInfo.email });
-    axios.post('http://localhost:3333/pay/monthly', token)
-    .then(res => {
-      console.log(res);
-    })
-    .catch(err => {
-      console.log(err);
-    })
-  }
+    let { token } = await this.props.stripe.createToken({
+      email: this.props.context.userInfo.email
+    });
+    axios
+      .post("http://localhost:3333/pay/monthly", token)
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
 
   render() {
-    if (this.state.complete) return <h1>Purchase Complete</h1>
+    if (this.state.complete) return <h1>Purchase Complete</h1>;
     return (
       <div>
         <Navbar
@@ -34,10 +39,12 @@ class Billing extends Component {
           <Sidebar />
           <div className="title-div">
             <h1>Billing</h1>
+            <p>Would You Like To Become a Member?</p>
             <div className="stripe">
-              <p>Would you like to complete a purchase?</p>
-              <CardElement />
-              <button onClick={this.submit}>Send</button>
+              <div className="card-element">
+                <CardElement />
+              </div>
+              <button onClick={this.submit}>Monthly Subscriptions - $4.99</button>
             </div>
           </div>
         </div>
