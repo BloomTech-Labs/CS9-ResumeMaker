b1edcafd06c783dc244838c46d0209eccd882767
ktan114 <ktaneight@gmail.com>
2018-08-22 17:19:03 -0400

    Have submit button to complete monthly subscriptions

--- client/src/components/billing.js
+++ client/src/components/billing.js
@@ -1,15 +1,27 @@
 import React, { Component } from "react";
 import { CardElement, injectStripe } from "react-stripe-elements";
+import axios from 'axios';
 import Sidebar from "./subComponents/sidebar";
 import Navbar from "./subComponents/navbar";
 
 class Billing extends Component {
+  state = {
+    complete: false
+  }
 
-  submit = () => {
-
+  submit = async () => {
+    let { token } = await this.props.stripe.createToken({ email: this.props.context.userInfo.email });
+    axios.post('http://localhost:3333/pay/monthly', token)
+    .then(res => {
+      console.log(res);
+    })
+    .catch(err => {
+      console.log(err);
+    })
   }
 
   render() {
+    if (this.state.complete) return <h1>Purchase Complete</h1>
     return (
       <div>
         <Navbar
