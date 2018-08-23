62a7f6ff20b803bae0d953b54077adfe81120084
ktan114 <ktaneight@gmail.com>
2018-08-23 17:58:02 -0400

    Change save to update

--- user/UserRoutes.js
+++ user/UserRoutes.js
@@ -176,6 +176,33 @@ UserRouter.post("/login", (req, res) => {
       if (!user) {
         return res.status(401).json({ errorMessage: "Invalid credentials." });
       }
+
+      if (user.membership) {
+        stripe.subscriptions.retrieve(user.subscriptions, (err, sub) => {
+          if (err) console.log(err);
+          if (
+            sub.status === "canceled" ||
+            sub.status === "past_due" ||
+            sub.status === "unpaid"
+          ) {
+            stripe.subscriptions.del(user.subscriptions, (err, success) => {
+              if (err) console.log(err);
+              else console.log(success);
+            })
+            User.findOneAndUpdate(
+              { email },
+              { membership: false, subscription: null }
+            )
+              .then(user => {
+                console.log(user);
+              })
+              .catch(err => {
+                console.log("Unable to find");
+              });
+          }
+        });
+      }
+
       user
         .checkPassword(password)
         .then(verified => {
@@ -185,6 +212,9 @@ UserRouter.post("/login", (req, res) => {
               email: user.email,
               password: user.password
             };
+            // if (user.membership) {
+            //   payload.resumes = user.resumes;
+            // }
             const token = jwt.sign(payload, secretKey, {
               expiresIn: "7d"
             });
