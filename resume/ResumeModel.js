c46d9c9014ce95e39ff668b37c0695cb34940406
ktan114 <ktaneight@gmail.com>
2018-08-24 18:39:18 -0400

    Built the resume model

--- resume/ResumeModel.js
+++ resume/ResumeModel.js
@@ -1,8 +1,56 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const ObjectId = Schema.Types.ObjectId;
 
 const Resume = new Schema({
-})
+  user: {
+    type: ObjectId,
+    ref: "User"
+  },
+  email: String,
+  name: {
+    firstname: String,
+    middlename: String,
+    lastname: String
+  },
+  location: String,
+  title: String,
+  phonenumber: String,
+  links: {
+    linkedin: String,
+    github: String,
+    portfolio: String
+  },
+  sections: {
+    experience: [
+      {
+        title: String,
+        company: String,
+        location: String,
+        from: String,
+        to: String
+      }
+    ],
+    education: [
+      {
+        school: String,
+        degree: String,
+        fieldofstudy: String,
+        from: String,
+        to: String
+      }
+    ],
+    summary: [
+      {
+        type: String
+      }
+    ],
+    skills: [
+      {
+        type: String
+      }
+    ]
+  }
+});
 
 module.exports = mongoose.model("Resume", Resume);
