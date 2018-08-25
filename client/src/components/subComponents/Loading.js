2b60ef5e35beda85c9420fc78708a6edd38c213c
ktan114 <ktaneight@gmail.com>
2018-08-25 00:29:11 -0400

    Changed rendering text

--- client/src/components/subComponents/Loading.js
+++ client/src/components/subComponents/Loading.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default () => {
   return (
     <div>
-      <p className="fa fa-spinner fa-spin" /> One Moment While We Subscribe You...
+      <p className="fa fa-spinner fa-spin" /> One Moment While We Process This...
     </div>
   )
 }
