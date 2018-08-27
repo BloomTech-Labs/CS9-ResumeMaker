74bbe2b0315ac84daeb45877d13fb112f904fadd
ktan114 <ktaneight@gmail.com>
2018-08-27 13:22:54 -0400

    Create a POST request to create a new resume and save ref to user

--- resume/ResumeRouter.js
+++ resume/ResumeRouter.js
@@ -1,14 +1,39 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const passport = require("passport");
+
+const Resume = require("./ResumeModel");
+const User = require("../user/UserModel");
 
 /*
     @route  resume
     @desc   Creates a new resume per user
     @access Private (Production) | Public (Development)
 */
-router.post("/", passport.authenticate("jwt", { session: false }), (req, res) => {
-    
-})
+router.post(
+  "/",
+  //   passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const newResume = new Resume(req.body);
+    const { email } = req.body;
+    // const email = req.user.email;
+
+    newResume
+      .save()
+      .then(resume => {
+        User.findOneAndUpdate({ email })
+          .then(user => {
+            user.resumes.push(resume);
+          })
+          .catch(err => {
+            res.status(400).json({ Error: err });
+          });
+        res.status(201).json({ Resume: resume });
+      })
+      .catch(err => {
+        res.status(400).json({ Error: err });
+      });
+  }
+);
 
 module.exports = router;
