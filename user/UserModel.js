eaa338626fcf297eb545a25a458691fc44fb3ab7
ktan114 <ktaneight@gmail.com>
2018-08-27 16:59:39 -0400

    Adjusted prehook save method to bypass hash if password is not changed

--- user/UserModel.js
+++ user/UserModel.js
@@ -178,6 +178,7 @@ const User = new mongoose.Schema(
 );
 
 User.pre("save", function(next) {
+  if (!this.isModified('password')) return next();
   bcrypt.hash(this.password, 11, (err, hash) => {
     if (err) return next(err);
     this.password = hash;
