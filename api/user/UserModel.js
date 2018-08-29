94d86709facebc48ff2006ef89e07f61a28a83e0
ktan114 <ktaneight@gmail.com>
2018-08-29 12:58:22 -0400

    Prettier

--- api/user/UserModel.js
+++ api/user/UserModel.js
@@ -1,66 +1,13 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
-<<<<<<< HEAD:api/user/UserModel.js
 const ObjectId = mongoose.Schema.Types.ObjectId;
-const { checkPasswordStrength, validateEmail, validatePhone, validateLinkedIn, validateGithub } = require("../helpers/Validation");
-=======
-
-// Validation
-
-/*
-  Email example: test@service.com
-*/
-const validateEmail = email => {
-  const re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  return re.test(email);
-};
-
-/*
-  Phone Number example: 123-456-7890
-*/
-// const validatePhone = number => {
-//   const re = /^([0-9]{3}-)([0-9]{3}-)([0-9]{4})$/g;
-//   return re.test(number);
-// };
-
-/* 
-  Password Requirements:
-    Must be longer than 6 characters
-    Must have at least 1 uppercase
-    Must have at least 1 lowercase
-    Must have at least 1 special character
-    Must have at least 1 digit
-*/
-const checkPasswordStrength = password => {
-  const minlength = 6;
-
-  if (password.length < minlength) return false;
-  if (!password.match(/[A-Z]/)) return false;
-  if (!password.match(/[a-z]/)) return false;
-  if (!password.match(/\d/)) return false;
-  if (!password.match(/[`~!@#$%^&*\(\)_\-\+=\[{\]}\|\\:;"'<,>\.\?\/]/))
-    return false;
-  return true;
-};
-
-// Checking links on the backend is a waste of processing time and creates unnecessary validation errors
-
-/*
-  Linkedin example: linkedin.com/in/test/ (allows some special characters)
-*/
-// const validateLinkedIn = url => {
-//   const re = /^(linkedin\.com\/in\/[\w-!@#$%^&*]+)$/;
-//   return re.test(url);
-// };
-
-/*
-  GitHub example: github.com/test/ (allows some special characters)
-*/
-// const validateGithub = url => {
-//   const re = /^(github\.com\/[\w-!@#$%^&*]+)$/;
-//   return re.test(url);
-// };
->>>>>>> f9712a4a20682555ebddbbebe7b4d088dc29545f:user/UserModel.js
+const {
+  checkPasswordStrength,
+  validateEmail,
+  validatePhone,
+  validateLinkedIn,
+  validateGithub
+} = require("../helpers/Validation");
 
 const User = new mongoose.Schema(
   {
@@ -189,16 +136,18 @@ const User = new mongoose.Schema(
       default: false
     },
     subscription: String,
-    resumes: [{
-      type: ObjectId,
-      ref: "Resume"
-    }]
+    resumes: [
+      {
+        type: ObjectId,
+        ref: "Resume"
+      }
+    ]
   },
   { timestamps: true }
 );
 
 User.pre("save", function(next) {
-  if (!this.isModified('password')) return next();
+  if (!this.isModified("password")) return next();
   bcrypt.hash(this.password, 11, (err, hash) => {
     if (err) return next(err);
     this.password = hash;
