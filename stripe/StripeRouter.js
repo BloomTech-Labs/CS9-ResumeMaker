115af694417b382c1b362088b610e82b1c6f6929
ktan114 <ktaneight@gmail.com>
2018-08-28 23:32:47 -0400

    Removed helper functions from StripeRouter

--- stripe/StripeRouter.js
+++ stripe/StripeRouter.js
@@ -5,41 +5,7 @@ require("dotenv").config();
 const stripe = require("stripe")(process.env.SECRET_KEY);
 
 const User = require("../user/UserModel");
-
-const checkMembership = email => {
-  User.findOne({ email })
-    .then(async user => {
-      return await user.membership;
-    })
-    .catch(async err => {
-      return await err;
-    });
-};
-
-const createCustomer = async (email, token) => {
-  const customer = stripe.customers.create({
-    email: email,
-    source: token
-  });
-  return await customer;
-};
-
-const createSubscription = async (id, planType) => {
-  const subscription = stripe.subscriptions.create({
-    customer: id,
-    items: [
-      {
-        plan: planType
-      }
-    ]
-  });
-  return await subscription;
-};
-
-const changeStatus = async (email, newInfo) => {
-  const updatedStatus = await User.findOneAndUpdate({ email }, newInfo);
-  return updatedStatus;
-};
+const { checkMembership, createCustomer, createSubscription, changeStatus } = require("../helpers/Stripe");
 
 /*
     @route  POST pay/monthly
