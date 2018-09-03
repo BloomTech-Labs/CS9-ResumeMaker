e6d0079ae69edb02542b31fdc76ca95365047696
ktan114 <ktaneight@gmail.com>
2018-09-03 17:48:48 -0400

    Set up html2canvas to target template1

--- client/src/components/PDF/PDF.js
+++ client/src/components/PDF/PDF.js
@@ -4,6 +4,14 @@ import jsPDF from "jspdf";
 
 class PDF extends Component {
   printDocument = () => {
+    let input = document.getElementsByClassName("template1");
+    html2canvas(input[0])
+      .then(canvas => {
+        const imgData = canvas.toDataURL("image/png");
+      })
+      .catch(err => {
+        console.log(err);
+      });
   };
 
   render() {
