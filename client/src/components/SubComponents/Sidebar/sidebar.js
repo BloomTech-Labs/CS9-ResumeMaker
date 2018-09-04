cb6430c7d346c6c6382f9c7394c0952b30db7c22
Natalie <natalieschennum@gmail.com>
2018-09-04 15:02:18 -0700

    adding icons to sidbar again

--- client/src/components/SubComponents/Sidebar/sidebar.js
+++ client/src/components/SubComponents/Sidebar/sidebar.js
@@ -49,44 +49,6 @@ class Sidebar extends Component {
     }
     return (
       <div className="sidebar">
-<<<<<<< HEAD
-        <div className="static-sidebar" style={{fontFamily: "Verdana",   fontSize: "0.7rem", fontWeight: "550", }}>
-          <Link to="/templates" className="sidebar-button">
-            {" "}
-            <i class="fa fa-copy sm"/> Templates
-          </Link>
-          <Link to="/resumes" className="sidebar-button">
-            {" "}
-            <i class="fa fa-file-alt sm"/>Resumes
-          </Link>
-          <Link to="/jobTitle" className="sidebar-button">
-            {" "}
-            <i className="fa fa-briefcase sm"/>Job Title
-          </Link>
-          <Link to="/summary" className="sidebar-button">
-            {" "}
-            <i class="fa fa-edit sm"/>Summary
-          </Link>
-          <Link to="/skills" className="sidebar-button">
-            {" "}
-            <i class="fa fa-toolbox sm"/>Skills
-          </Link>
-          <Link to="/experience" className="sidebar-button">
-            {" "}
-            <i class="fa fa-lightbulb sm"/>Experience
-          </Link>
-          <Link to="/education" className="sidebar-button">
-            {" "}
-            <i class="fr fa-graduation-cap sm"/>Education
-          </Link>
-          <Link to="/billing" className="sidebar-button">
-            {" "}
-            <i class="fa fa-credit-card sm"/>Billing
-          </Link>
-          <Link to="/settings" className="sidebar-button">
-            {" "}
-            <i class="fa fa-sliders-h sm"/>Settings
-=======
         <div
           className="static-sidebar"
           style={{
@@ -101,7 +63,8 @@ class Sidebar extends Component {
               active: window.location.pathname.includes("/templates")
             })}
           >
-            Templates
+              {" "}
+            <div class="fa fa-copy sm"style={{color:"white"}}/> {" "} Templates
           </Link>
           <Link
             to="/resumes"
@@ -109,7 +72,8 @@ class Sidebar extends Component {
               active: window.location.pathname.includes("/resumes")
             })}
           >
-            Resumes
+            {" "}
+            <div class="fa fa-file-alt sm"style={{color:"white"}}/> {" "} Resumes
           </Link>
           <Link
             to="/jobtitle"
@@ -117,7 +81,7 @@ class Sidebar extends Component {
               active: window.location.pathname.includes("/jobtitle")
             })}
           >
-            Job Title
+             <div className="fa fa-briefcase sm"style={{color:"white"}}/> {" "}Job Title
           </Link>
           <Link
             to="/summary"
@@ -125,7 +89,8 @@ class Sidebar extends Component {
               active: window.location.pathname.includes("/summary")
             })}
           >
-            Summary
+            {" "}
+            <div class="fa fa-edit sm"style={{color:"white"}}/> {" "} Summary
           </Link>
           <Link
             to="/skills"
@@ -133,7 +98,8 @@ class Sidebar extends Component {
               active: window.location.pathname.includes("/skills")
             })}
           >
-            Skills
+           {" "}
+            <div class="fa fa-wrench" style={{color:"white"}}/> {" "} Skills
           </Link>
           <Link
             to="/experience"
@@ -141,7 +107,8 @@ class Sidebar extends Component {
               active: window.location.pathname.includes("/experience")
             })}
           >
-            Experience
+            {" "}
+            <div class="fa fa-lightbulb sm"style={{color:"white"}}/> {" "} Experience
           </Link>
           <Link
             to="/education"
@@ -149,7 +116,8 @@ class Sidebar extends Component {
               active: window.location.pathname.includes("/education")
             })}
           >
-            Education
+            {" "}
+            <div class="fa fa-graduation-cap sm" style={{color:"white"}}/> {" "}Education
           </Link>
           <Link
             to="/billing"
@@ -157,7 +125,8 @@ class Sidebar extends Component {
               active: window.location.pathname.includes("/billing")
             })}
           >
-            Billing
+           {" "}
+            <div class="fa fa-credit-card sm"style={{color:"white"}}/> {" "} Billing
           </Link>
           <Link
             to="/settings"
@@ -165,8 +134,8 @@ class Sidebar extends Component {
               active: window.location.pathname.includes("/settings")
             })}
           >
-            Settings
->>>>>>> 48c15dc9cda04e46c0d5674e3cbe8243b5cd98b3
+            {" "}
+            <div class="fa fa-sliders-h sm"style={{color:"white"}}/> {" "}Settings
           </Link>
         </div>
         <Route
