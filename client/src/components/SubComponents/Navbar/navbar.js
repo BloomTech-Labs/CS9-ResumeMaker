cd09144b09403b7a64f7796c10ec4293de21f14a
Natalie <natalieschennum@gmail.com>
2018-09-05 15:59:04 -0700

    Deleting Unused Syntax

--- client/src/components/SubComponents/Navbar/navbar.js
+++ client/src/components/SubComponents/Navbar/navbar.js
@@ -7,27 +7,6 @@ class Navbar extends Component {
   render() {
     return (
       <nav className="navbar sticky-top mb-0 d-flex justify-content-end">
-          
-        {/* <ol className="breadcrumb p-1 mb-0 bg-dark ">
-        <Link className="knockout" style={{fontSize: '1.3rem'}} to="/">rezLeft</Link>
-          {this.props.breadcrumbs.map((item, index) => (
-            <li className="breadcrumb-item" key={index}>
-              <Link
-                className="breadcrumb-item"
-                style={{
-                  color: "#45A29E",
-                  fontFamily: "Verdana",
-                  fontSize: "0.7rem", 
-                  fontWeight: "550", 
-
-                }}
-                to={item.link}
-              >
-                {item.title}
-              </Link>
-            </li>
-          ))}
-        </ol> */}
         <Route 
           render={({ history }) => (
             <div
