9e0e8ada508dac4a2a05e81b85e2dc7788fc2422
ktan114 <ktaneight@gmail.com>
2018-09-08 01:06:28 -0400

    Change quote on summary page for the time being

--- client/src/components/Summary/summary.js
+++ client/src/components/Summary/summary.js
@@ -13,47 +13,50 @@ class Summary extends Component {
   componentWillUnmount() {
     this.props.context.actions.expandResumeIDs(
       this.props.context.userInfo.currentResume
-    )
+    );
   }
 
   render() {
     return (
       <div>
-         <Navbar context={this.props.context}/>
+        <Navbar context={this.props.context} />
         <div className="overall-component-div row">
-       
           <Sidebar context={this.props.context} />
           <div className="title-div col">
             <div className="link-hide">
-              <h4>
-                PERSONAL SUMMARY{" "}</h4>
-                <Link
-                  to={{
-                    pathname: "/summary/create", // component being Linked to
-                    state: { index: false } // Setting Index passed into educationCreate component - false means new
-                  }}
-                >
-                  <i className="fa fa-pencil fa-lg"/>
-                </Link>
+              <h4>PERSONAL SUMMARY </h4>
+              <Link
+                to={{
+                  pathname: "/summary/create", // component being Linked to
+                  state: { index: false } // Setting Index passed into educationCreate component - false means new
+                }}
+              >
+                <i className="fa fa-pencil fa-lg" />
+              </Link>
             </div>
-            <p style={{ fontSize: "0.7rem", paddingLeft: ".6rem", borderTop: "1px solid black", width: "100%"}}>
-              Click the pencil to add a Personal Summary. 
-              It should be aimed at the position you are seeking
-              and contain something about the past, present, and future.{" "}
+            <p
+              style={{
+                fontSize: "0.7rem",
+                paddingLeft: ".6rem",
+                borderTop: "1px solid black",
+                width: "100%"
+              }}
+            >
+              Click the pencil to add a Personal Summary.{" "}
             </p>
 
             <div className="summary-containment-div">
               {this.props.context.userInfo.summary.map((element, index) => {
                 return (
-                    <ItemCard
-                      linkTo="/summary"
-                      elementName="summary"
-                      putPath="sections.summary"
-                      index={index}
-                      key={index}
-                      content={element.content}
-                      context={this.props.context}
-                    />
+                  <ItemCard
+                    linkTo="/summary"
+                    elementName="summary"
+                    putPath="sections.summary"
+                    index={index}
+                    key={index}
+                    content={element.content}
+                    context={this.props.context}
+                  />
                 );
               })}
             </div>
