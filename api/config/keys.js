002832aa9c61915a48a44849b34106f2b46407df
ktan114 <ktaneight@gmail.com>
2018-09-13 19:41:25 -0400

    Remove files

--- api/config/keys.js
+++
@@ -1,8 +0,0 @@
-require("dotenv").config();
-
-module.exports = {
-  uri: process.env.MLABURL,
-  secret: process.env.SECRET,
-  secret_key: process.env.SECRET_KEY,
-  site_name: process.env.SITE_NAME
-};
