9adad1cf250919d9aa7e6086dcf7b9137e3b64d0
Natalie <natalieschennum@gmail.com>
2018-09-16 18:03:12 -0700

    removed navbar padding

--- client/src/components/SubComponents/Navbar/navbar.js
+++ client/src/components/SubComponents/Navbar/navbar.js
@@ -4,12 +4,12 @@ import { Route } from "react-router-dom";
 class Navbar extends Component {
   render() {
     return (
-      <nav className="navbar sticky-top mb-0 d-flex " style={{padding:".3rem"}}>
+      <nav className="navbar sticky-top p-0 mb-0 d-flex " style={{padding:".3rem"}}>
       <h5 className="nav-header" style={{float: "left", color: "lightgray", margin: "0", paddingLeft: "1.5rem", fontWeight: "800", textShadow: "-1px -1px 1px black"}}>rezRight</h5>
         <Route 
           render={({ history }) => (
             <div
-              style={{ fontSize: "0.8rem", fontFamily: "Verdana", color: "#F2F2F0", textShadow: "1px 1px black", paddingRight: "1rem"}}
+              style={{ fontSize: "0.8rem", fontFamily: "Verdana", color: "#F2F2F0", textShadow: "1px 1px black"}}
               className="logout-btn logout btn justify-content-end"
               onClick={() => {
                 this.props.context.actions.setLogout();
